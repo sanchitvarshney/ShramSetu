30621acb9ef24a20169ff7a9d5606376e5645d4c
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-02-09 11:48:42 +0530

    refactories code

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -28,6 +28,7 @@ const Login: React.FC = () => {
   const navigate = useNavigate();
   const { toast } = useToast();
 
+  
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     dispatch(login({ userName: email, password })).then((response: any) => {
